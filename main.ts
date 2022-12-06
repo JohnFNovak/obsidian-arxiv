de64a208d5a3d712fd9fa9465645a9d44cbe46bd
John Novak <john.franc.novak@gmail.com>
2022-12-06 13:11:54 -0800

    close to working...

--- main.ts
+++ main.ts
@@ -1,81 +1,130 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	TextComponent,
+	PluginSettingTab,
+	Setting,
+	RequestParam,
+	request
+} from 'obsidian';
+
+// import parser from 'xml2json';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface arXivSummary {
+	id: string;
+	updated: string;
+	published: string;
+	title: string;
+	authors: [string];
+	summary: string;
+	// url: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface arXivSettings {
+	template: string;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const DEFAULT_SETTINGS: arXivSettings = {
+	template: `{{title}}\n{{authors}}\n{{summary}}`,
+}
+
+export default class arXivPlugin extends Plugin {
+	settings: arXivSettings;
+
+	getUrl(): string {
+		return `https://export.arxiv.org/api/query?id_list=`;
+	}
+
+	async getArXivPaperByID(editor: Editor) {
+		new ArXiVIDModal(this.app, this, editor).open();
+	}
+
+	handleNotFound(paperID: string) {
+		new Notice(`${paperID} not found on arXiv.`);
+	}
+
+	handleCouldntResolveDisambiguation() {
+		new Notice(`Could not automatically resolve disambiguation.`);
+	}
+
+	formatExtractInsert(summary: arXivSummary): string {
+		const template = this.settings.template;
+		const formattedTemplate = template
+		  .replace("{{title}}", summary.title)
+		  .replace("{{id}}", summary.id)
+		  .replace("{{summary}}", summary.summary)
+		//   .replace("{{authors}}", summary.authors.join(', '))
+		  .replace("{{updated}}", summary.updated)
+		  .replace("{{published}}", summary.published);
+		return formattedTemplate;
+	}
+
+	async createPage(editor: Editor, paperID: string) {
+		let extract: arXivSummary = await this.getPaperDetails(paperID);
+		if (extract.title == `Error`) {
+			this.handleNotFound(paperID);
+			return;
+		}
+		editor.replaceSelection(this.formatExtractInsert(extract));
+	}
+
+	parseResponse(json: any): arXivSummary {
+		console.log(json);
+		const entries = json['feed']['entry'];
+		console.log(entries);
+		const papers: arXivSummary[] = entries.map((entry) => {
+			const authors = entry['authors'].map((author) => {author['name']})
+			const paper: arXivSummary = {
+				id: entry['id'],
+				updated: entry['updated'],
+				published: entry['published'],
+				title: entry['title'],
+				authors: authors,
+				summary: entry['summary'],
+			};
+			return paper;
+		});
+		return papers[0];
+	}
+
+	async getPaperDetails(paperID: string): Promise<arXivSummary> {
+		const url = this.getUrl() + encodeURIComponent(paperID);
+		const requestParam: RequestParam = {
+			url: url,
+		};
+		const parser = new DOMParser();
+		const resp = await request(requestParam)
+		// .then((r) => console.log(r))
+		.then((r) => parser.parseFromString(r, "text/xml"))
+		.then((r) => console.log(r))
+		.catch(
+			() =>
+			new Notice(
+				"Failed to get arXiv. Check your internet connection or language prefix."
+				)
+		);
+		const extract = this.parseResponse(resp);
+		return extract;
+	}
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+			id: "arxiv-get-by-id",
+			name: "Get arXiv paper by ID",
+			editorCallback: (editor: Editor) =>
+			this.getArXivPaperByID(editor),
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new arXivSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -91,47 +140,85 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class ArXiVIDModal extends Modal {
+	paperID: string;
+	plugin: arXivPlugin;
+	editor: Editor;
+
+	constructor(app: App, plugin: arXivPlugin, editor: Editor) {
 		super(app);
+		this.plugin = plugin;
+		this.editor = editor;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		let { contentEl } = this;
+
+		contentEl.createEl("h2", { text: "Enter Paper ID:" });
+
+		const inputs = contentEl.createDiv("inputs");
+		const searchInput = new TextComponent(inputs).onChange((paperID) => {
+			this.paperID = paperID;
+		});
+		searchInput.inputEl.focus();
+		searchInput.inputEl.addEventListener("keydown", (event) => {
+			if (event.key === "Enter") {
+				this.close();
+			}
+		});
+
+		const controls = contentEl.createDiv("controls");
+		const searchButton = controls.createEl("button", {
+			text: "Search",
+			cls: "mod-cta",
+			attr: {
+				autofocus: true,
+			},
+		});
+		searchButton.addEventListener("click", this.close.bind(this));
+		const cancelButton = controls.createEl("button", { text: "Cancel" });
+		cancelButton.addEventListener("click", this.close.bind(this));
 	}
 
-	onClose() {
-		const {contentEl} = this;
+	async onClose() {
+		let { contentEl } = this;
+
 		contentEl.empty();
+		if (this.paperID) {
+			await this.plugin.createPage(this.editor, this.paperID);
+		}
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class arXivSettingTab extends PluginSettingTab {
+	plugin: arXivPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: arXivPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'Obsidian arXiv' });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		.setName("arXiv Extract Template")
+		.setDesc(
+		  `Set markdown template for extract to be inserted.\n
+		  Available template variables are {{title}}, {{id}}, {{summary}}, {{authors}}, {{updated}}, and {{published}}.
+		  `
+		)
+		.addTextArea((textarea) =>
+		  textarea
+			.setValue(this.plugin.settings.template)
+			.onChange(async (value) => {
+			  this.plugin.settings.template = value;
+			  await this.plugin.saveSettings();
+			})
+		);
 	}
 }
